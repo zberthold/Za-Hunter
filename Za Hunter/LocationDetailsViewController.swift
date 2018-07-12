0ece6f84e56fb8bfe5aed0e9b451abd466122fec
Zoe Berthold <zoeberthold@zoes-macbook-pro.local>
2018-07-12 17:11:16 -0500

    collected and passed data in segue

--- Za Hunter/LocationDetailsViewController.swift
+++ Za Hunter/LocationDetailsViewController.swift
@@ -7,29 +7,24 @@
 //
 
 import UIKit
+import MapKit
 
 class LocationDetailsViewController: UIViewController {
 
+    var selectedMapItem = MKMapItem()
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(selectedMapItem.name!)
 
-        // Do any additional setup after loading the view.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
 
 }
--- Za Hunter/ViewController.swift
+++ Za Hunter/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     let locationManager = CLLocationManager()
     @IBOutlet weak var mapView: MKMapView!
     var region = MKCoordinateRegion()
+    var mapItems = [MKMapItem]()
+    var selectedMapItem = MKMapItem()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,6 +53,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
                         annotation.coordinate = mapItem.placemark.coordinate
                         annotation.title = mapItem.name
                         self.mapView.addAnnotation(annotation)
+                        self.mapItems.append(mapItem)
                 }
             }
         }
@@ -74,6 +77,21 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     
     func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
         performSegue(withIdentifier: "ShowLocationDetailsSegue", sender: nil)
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        for mapItem in mapItems {
+            if mapItem.placemark.coordinate.latitude == view.annotation?.coordinate.latitude &&
+                mapItem.placemark.coordinate.longitude == view.annotation?.coordinate.longitude {
+                selectedMapItem = mapItem
+            }
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? LocationDetailsViewController {
+            destination.selectedMapItem = selectedMapItem
+        }
     }
 
 
